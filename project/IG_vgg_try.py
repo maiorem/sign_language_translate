cd198a71e250728240457c34c12329846ec89963
Seyoung Hong <maiorem00@gmail.com>
2020-11-28 23:59:19 +0900

    전이학습 성공

--- project/IG_vgg_try.py
+++ project/IG_vgg_try.py
@@ -6,16 +6,7 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_im
 from tensorflow.python.keras.utils.data_utils import Sequence
 from tensorflow.keras.applications.vgg16 import VGG16
 
-train_datagen = ImageDataGenerator(rescale=1./255, 
-                                horizontal_flip=True,
-                                vertical_flip=True,
-                                width_shift_range=0.1,
-                                height_shift_range=0.1,
-                                rotation_range=40,
-                                zoom_range=0.2,
-                                shear_range=0.2,
-                                fill_mode='nearest'
-                                )
+train_datagen = ImageDataGenerator(rescale=1./255)
 test_datagen = ImageDataGenerator(rescale=1./255)
 # validation_datagen=ImageDataGenerator(rescale=1./255)
 
@@ -31,20 +22,18 @@ xy_test=test_datagen.flow_from_directory(
     './data/test',
     target_size=(200,200),
     batch_size=16,
-    class_mode='categorical' 
+    class_mode=None,
+    shuffle=False 
 )
 
 
 model = VGG16(include_top=False, weights='imagenet',input_shape = (200,200,3))
 
-
-# 이미지를 모델에 입력시켜 결과를 가져옵니다. 본래 어떤 예측 결과가 출력되어야 하지만 모델의 일부만 가져왔기 때문에 병목 특징이 출력됩니다.
 bottleneck_features_train = model.predict_generator(xy_train, 7200)
-# 출력된 병목 데이터를 저장합니다.
-np.save(open('./saveDATA/bottleneck_features_train.npy', 'w'), bottleneck_features_train)
+np.save(open('./saveDATA/bottleneck_features_train.npy', 'wb'), bottleneck_features_train)
 
 bottleneck_features_validation = model.predict_generator(xy_test, 1000)
-np.save(open('./saveDATA/bottleneck_features_validation.npy', 'w'), bottleneck_features_validation)
+np.save(open('./saveDATA/bottleneck_features_validation.npy', 'wb'), bottleneck_features_validation)
 
 # model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
--- project/model_transfer.py
+++ project/model_transfer.py
@@ -1,26 +1,60 @@
 import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.utils import to_categorical
 
-train_data = np.load(open('bottleneck_features_train.npy'))
-# 앞서 언급한 바와 같이 병목 특징은 순서대로 추출되기 때문에 라벨 데이터는 아래와 같이 손쉽게 생성할 수 있습니다.
-train_labels = np.array([0] * 1000 + [1] * 1000)
+import matplotlib.pyplot as plt
 
-validation_data = np.load(open('bottleneck_features_validation.npy'))
-validation_labels = np.array([0] * 400 + [1] * 400)
+train_data = np.load(open('./saveDATA/bottleneck_features_train.npy', 'rb'))
+train_labels = np.array([0] * 720 + [1] * 720 + [2] * 720 + [3] * 720 + [4] * 720 + [5] * 720 + [6] * 720 + [7] * 720 + [8] * 720 + [9] * 720)
+
+validation_data = np.load(open('./saveDATA/bottleneck_features_validation.npy', 'rb'))
+validation_labels = np.array([0] * 100 + [1] * 100 + [2] * 100 + [3] * 100 + [4] * 100 + [5] * 100 + [6] * 100 + [7] * 100 + [8] * 100 + [9] * 100)
+
+print(train_data.shape) #(7200, 6, 6, 512)
+print(train_labels.shape) #(7200,)
+
+train_labels=to_categorical(train_labels) 
+validation_labels=to_categorical(validation_labels)
 
 model = Sequential()
 model.add(Flatten(input_shape=train_data.shape[1:]))
 model.add(Dense(256, activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(1, activation='softmax'))
+model.add(Dense(10, activation='softmax'))
 
 model.compile(optimizer='adam',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(train_data, train_labels,
+history=model.fit(train_data, train_labels,
           epochs=50,
           batch_size=16,
           validation_data=(validation_data, validation_labels))
 model.save_weights('./save_weights/bottleneck_fc_model.h5')
+
+loss, accuracy=model.evaluate(validation_data, validation_labels, batch_size=16)
+print('loss :', loss)
+print('acc :', accuracy)
+'''
+loss : 0.3532889783382416
+acc : 0.9779999852180481
+'''
+
+acc=history.history['accuracy']
+val_acc=history.history['val_accuracy']
+loss=history.history['loss']
+val_loss=history.history['val_loss']
+
+plt.plot(acc)
+plt.plot(val_acc)
+plt.plot(loss)
+plt.plot(val_loss)
+
+plt.title('loss & acc')
+plt.ylabel('loss, acc')
+plt.xlabel('epoch')
+
+plt.legend(['loss', 'val_loss', 'acc', 'val_acc'])
+plt.show()
+
