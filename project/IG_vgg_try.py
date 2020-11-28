a02bd80a2bce19c5c1cc1c1f3a2080aab67cef1e
Seyoung Hong <maiorem00@gmail.com>
2020-11-28 22:11:58 +0900

    transfer try

--- project/IG_vgg_try.py
+++ project/IG_vgg_try.py
@@ -4,6 +4,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, Dense, Flatten, MaxPooling2D, Dropout
 from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 from tensorflow.python.keras.utils.data_utils import Sequence
+from tensorflow.keras.applications.vgg16 import VGG16
 
 train_datagen = ImageDataGenerator(rescale=1./255, 
                                 horizontal_flip=True,
@@ -20,92 +21,55 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 
 xy_train=train_datagen.flow_from_directory(
     './data/train',
-    target_size=(150,150),
+    target_size=(200,200),
     batch_size=16,
     class_mode=None,
     shuffle=False 
 ) 
 
-
 xy_test=test_datagen.flow_from_directory(
     './data/test',
-    target_size=(150,150),
+    target_size=(200,200),
     batch_size=16,
     class_mode='categorical' 
 )
-# validation_generator = validation_datagen.flow_from_directory(
-#         './data/validation',
-#         target_size=(150, 150),
-#         batch_size=16,
-#         class_mode='categorical'
-# )
-
-# # 이미지 증폭 디버깅
-# img = load_img('./data/train/0/four sign fingers hand90.jpg') 
-# x = img_to_array(img)  
-# x = x.reshape((1,) + x.shape) 
-
-# i = 0
-# for batch in train_datagen.flow(x, 
-#                             batch_size=1,
-#                             save_to_dir='./preview', 
-#                             save_prefix='0', 
-#                             save_format='jpg'):
-#     i += 1
-#     if i > 20:
-#         break 
-
-# print(xy_train[0][0].shape) #(6480, 150, 150, 3)
-# print(xy_train[0][1].shape) #(6480, 9)
-# print(xy_test[0][0].shape) #(1080, 150, 150, 3)
-# print(xy_test[0][1].shape) #(1080, 9)
-
-# np.save('./saveDATA/train_x.npy', arr=xy_train[0][0])
-# np.save('./saveDATA/train_y.npy', arr=xy_train[0][1])
-# np.save('./saveDATA/test_x.npy', arr=xy_test[0][0])
-# np.save('./saveDATA/test_y.npy', arr=xy_test[0][1])
 
 
-model=Sequential()
-model.add(Conv2D(32, (3,3), activation='relu', input_shape=(150,150,3)))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(32, (3,3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(64, (3,3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Flatten())
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(9, activation='softmax'))
+model = VGG16(include_top=False, weights='imagenet',input_shape = (200,200,3))
 
 
 # 이미지를 모델에 입력시켜 결과를 가져옵니다. 본래 어떤 예측 결과가 출력되어야 하지만 모델의 일부만 가져왔기 때문에 병목 특징이 출력됩니다.
-bottleneck_features_train = model.predict_generator(generator, 2000)
+bottleneck_features_train = model.predict_generator(xy_train, 7200)
+# 출력된 병목 데이터를 저장합니다.
+np.save(open('./saveDATA/bottleneck_features_train.npy', 'w'), bottleneck_features_train)
 
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+bottleneck_features_validation = model.predict_generator(xy_test, 1000)
+np.save(open('./saveDATA/bottleneck_features_validation.npy', 'w'), bottleneck_features_validation)
 
-history=model.fit_generator(
-    xy_train,
-    steps_per_epoch=1000 // 16,
-    epochs=50,
-    validation_data=xy_test,
-    validation_steps=4
-)
-model.save_weights('./save_weights/fit_gen.h5')
+# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-acc=history.history['accuracy']
-val_acc=history.history['val_accuracy']
-loss=history.history['loss']
-val_loss=history.history['val_loss']
+# history=model.fit_generator(
+#     xy_train,
+#     steps_per_epoch=1000 // 16,
+#     epochs=50,
+#     validation_data=xy_test,
+#     validation_steps=4
+# )
+# model.save_weights('./save_weights/fit_gen.h5')
 
-plt.plot(acc)
-plt.plot(val_acc)
-plt.plot(loss)
-plt.plot(val_loss)
+# acc=history.history['accuracy']
+# val_acc=history.history['val_accuracy']
+# loss=history.history['loss']
+# val_loss=history.history['val_loss']
 
-plt.title('loss & acc')
-plt.ylabel('loss, acc')
-plt.xlabel('epoch')
+# plt.plot(acc)
+# plt.plot(val_acc)
+# plt.plot(loss)
+# plt.plot(val_loss)
 
-plt.legend(['loss', 'val_loss', 'acc', 'val_acc'])
-plt.show()
+# plt.title('loss & acc')
+# plt.ylabel('loss, acc')
+# plt.xlabel('epoch')
+
+# plt.legend(['loss', 'val_loss', 'acc', 'val_acc'])
+# plt.show()
--- project/model_transfer.py
+++ project/model_transfer.py
@@ -1,33 +1,26 @@
 import numpy as np
-from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten
-from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from tensorflow.keras.layers import Dense, Dropout, Flatten
 
+train_data = np.load(open('bottleneck_features_train.npy'))
+# 앞서 언급한 바와 같이 병목 특징은 순서대로 추출되기 때문에 라벨 데이터는 아래와 같이 손쉽게 생성할 수 있습니다.
+train_labels = np.array([0] * 1000 + [1] * 1000)
 
-x_train=np.load('./saveDATA/train_x.npy')
-y_train=np.load('./saveDATA/train_y.npy')
-x_test=np.load('./saveDATA/test_x.npy')
-y_test=np.load('./saveDATA/test_y.npy')
+validation_data = np.load(open('bottleneck_features_validation.npy'))
+validation_labels = np.array([0] * 400 + [1] * 400)
 
+model = Sequential()
+model.add(Flatten(input_shape=train_data.shape[1:]))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1, activation='softmax'))
 
-model=VGG16(weights='imagenet', input_shape=(224, 224, 3))
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
-top_model = Sequential()
-top_model.add(Flatten(input_shape=model.output_shape[1:]))
-top_model.add(Dense(256, activation='relu'))
-top_model.add(Dense(10, activation='softmax'))
-
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
-
-model.fit(x_train, y_train,
-              epochs=100,
-              batch_size=10,
-              validation_data=(x_test, y_test)) 
-
-loss, acc=model.evaluate(x_test, y_test, batch_size=10)
-
-print('loss :', loss)
-print('acc :', acc)
-
-
+model.fit(train_data, train_labels,
+          epochs=50,
+          batch_size=16,
+          validation_data=(validation_data, validation_labels))
+model.save_weights('./save_weights/bottleneck_fc_model.h5')
