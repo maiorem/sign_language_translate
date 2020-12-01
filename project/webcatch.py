d5b9eff48739e5133e6c4e85d9f0d1421ad70ebb
Seyoung Hong <maiorem00@gmail.com>
2020-12-01 14:47:33 +0900

    크롤링

--- project/webcatch.py
+++ project/webcatch.py
@@ -3,7 +3,7 @@ from urllib.request import urlopen
 from urllib.request import urlretrieve
 from urllib.parse import quote_plus
 from selenium import webdriver
-
+ 
 search=input('검색 : ')
 url=f'https://www.google.com/search?q={quote_plus(search)}&rlz=1C1CHZN_koKR926KR926&sxsrf=ALeKk02D62tpvik4raiNBDuxquZbgn-LJQ:1606378974764&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjNgamB5J_tAhXEFogKHfMHBU4Q_AUoAXoECAkQAw&biw=1920&bih=969#imgrc=upw4mNbY0uWHOM&imgdii=xhE_tdSVkbu9iM'
 
